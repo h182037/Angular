491c79d13b8665d9ac02c1baf5f170a758436c5c
h182037 <sindre.aalhus@gmail.com>
2019-11-17 12:08:13 +0100

    lul

--- src/app/in-memory-data.service.ts
+++ src/app/in-memory-data.service.ts
@@ -8,11 +8,21 @@ import { Injectable } from '@angular/core';
 export class InMemoryDataService implements InMemoryDbService {
   createDb() {
     const heroes = [
-      { id: 11, name: 'Banan' },
-      { id: 12, name: 'Eple' },
-      { id: 13, name: 'Appelsin' },
-      { id: 14, name: 'Kokosnøtt' },
-      { id: 15, name: 'Sko' },
+      { id: 11, name: 'Red Dead Redemption 2' },
+      { id: 12, name: 'Fifa 20' },
+      { id: 13, name: 'Hearthstone' },
+      { id: 14, name: 'The Witcher 3' },
+      { id: 15, name: 'The Last of Us' },
+      { id: 16, name: 'Cyberpunk' },
+      { id: 17, name: 'Death Stranding' },
+      { id: 18, name: 'Monster Hunter World' },
+      { id: 19, name: 'World of Warcraft' },
+      { id: 20, name: 'Fårtnait' },
+      { id: 21, name: 'Conkers Bad Fur day'},
+      { id: 22, name: 'League of Legends' },
+
+
+
     ];
     return {heroes};
   }
