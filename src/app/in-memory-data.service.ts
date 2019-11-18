5a6b1fd3dd6bd118af77fd145dab6fd5cdb47239
h182037 <sindre.aalhus@gmail.com>
2019-11-18 14:14:28 +0100

    dis

--- src/app/in-memory-data.service.ts
+++ src/app/in-memory-data.service.ts
@@ -8,18 +8,18 @@ import { Injectable } from '@angular/core';
 export class InMemoryDataService implements InMemoryDbService {
   createDb() {
     const games = [
-      { id: 11, name: 'Red Dead Redemption 2', description: "Riding horses", ratings: [4, 4, 5, 5, 5], score: 0, comments: []},
-      { id: 12, name: 'Fifa 20', description: "Ruining friendships",ratings: [4, 3, 5, 2, 5], score: 0, comments: []},
-      { id: 13, name: 'Hearthstone',description: "Being a nerd", ratings: [1, 1, 5, 5, 5], score: 0, comments: []},
-      { id: 14, name: 'The Witcher 3', description: "Wiedzmin",ratings: [4, 5, 5, 5, 5], score: 0, comments: []},
-      { id: 15, name: 'The Last of Us 2', description: "Long escorting quest",ratings: [4, 4, 5, 5, 5, 4, 5], score: 0, comments: []},
-      { id: 16, name: 'Cyberpunk', description: "its da future",ratings: [4, 4, 2, 5, 5, 3 , 5], score: 0, comments: []},
-      { id: 17, name: 'Death Stranding', description: "Walking simulator",ratings: [1, 4, 5, 5, 5, 4, 3], score: 0, comments: []},
-      { id: 18, name: 'Monster Hunter World', description: "Japaneese dragons",ratings: [3, 3, 3, 4, 5], score: 0, comments: []},
-      { id: 19, name: 'World of Warcraft',description: "Virginity and mountain dew",ratings: [4, 4, 5, 5, 5, 2, 2, 2], score: 0, comments: []},
-      { id: 20, name: 'Overwatch' , description: "Pew Pew Pew",ratings: [4, 4, 5, 5, 5, 5, 1, 1], score: 0, comments: []},
-      { id: 21, name: 'Conkers Bad Fur day', description: "Drunk squirrel", ratings: [4, 5, 5, 5, 5], score: 0, comments: []},
-      { id: 22, name: 'League of Legends', description: "4 buttons is all you need",ratings: [4, 4, 5, 3, 3], score: 0, comments: []}
+      { id: 1, name: 'Red Dead Redemption 2', description: "Riding horses", ratings: [4, 4, 5, 5, 5], score: 0, comments: []},
+      { id: 2, name: 'Fifa 20', description: "Ruining friendships",ratings: [4, 3, 5, 2, 5], score: 0, comments: []},
+      { id: 3, name: 'Hearthstone',description: "A lack of social skills", ratings: [1, 1, 5, 5, 5], score: 0, comments: []},
+      { id: 4, name: 'The Witcher 3', description: "Wiedzmin",ratings: [4, 5, 5, 5, 5], score: 0, comments: []},
+      { id: 5, name: 'The Last of Us 2', description: "Long escorting quest",ratings: [4, 4, 5, 5, 5, 4, 5], score: 0, comments: []},
+      { id: 6, name: 'Cyberpunk', description: "its da future",ratings: [4, 4, 2, 5, 5, 3 , 5], score: 0, comments: []},
+      { id: 7, name: 'Death Stranding', description: "Walking simulator",ratings: [1, 4, 5, 5, 5, 4, 3], score: 0, comments: []},
+      { id: 8, name: 'Monster Hunter World', description: "Japaneese dragons",ratings: [3, 3, 3, 4, 5], score: 0, comments: []},
+      { id: 9, name: 'World of Warcraft',description: "Nerds and mountain dew",ratings: [4, 4, 5, 5, 5, 2, 2, 2], score: 0, comments: []},
+      { id: 10, name: 'Overwatch' , description: "Pew Pew Pew",ratings: [4, 4, 5, 5, 5, 5, 1, 1], score: 0, comments: []},
+      { id: 11, name: 'Conkers Bad Fur day', description: "Drunk squirrel", ratings: [4, 5, 5, 5, 5], score: 0, comments: []},
+      { id: 12, name: 'League of Legends', description: "4 buttons is all you need",ratings: [4, 4, 5, 3, 3], score: 0, comments: []}
     ];
     games.forEach(game => {
       var sum = 0;
